fbf2d167f2c8e1cdb20d5440bcada4e8b0e8c393
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-11-27 09:39:14 +0100

    Add project files.

---
+++ LuftkvalitetTests/MeassurementTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Luftkvalitet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luftkvalitet.Tests
+{
+    [TestClass()]
+    public class MeassurementTests
+    {
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
