2dfb83e950dc8a9457e826f5edb70e316867a4a4
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-11-27 10:10:00 +0100

    Test run, no notes

--- LuftkvalitetTests/MeassurementTests.cs
+++ LuftkvalitetTests/MeassurementTests.cs
@@ -11,10 +11,33 @@ namespace Luftkvalitet.Tests
     [TestClass()]
     public class MeassurementTests
     {
+        Meassurement meassurement1;
+        Meassurement meassurement2;
+        Meassurement meassurement3;
+        Meassurement meassurement4;
+        Meassurement meassurement5;
+        Meassurement meassurement6;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            meassurement1 = new Meassurement() { Id = 1, CO2 = 0, Humidity = 100, Location = "Her", Time = DateTime.Now };
+            meassurement2 = new Meassurement() { Id = 2, CO2 = -1, Humidity = 69, Location = "Her", Time = DateTime.Now };
+            meassurement3 = new Meassurement() { Id = 3, CO2 = 650, Humidity = 101, Location = "Her", Time = DateTime.Now };
+            meassurement4 = new Meassurement() { Id = 4, CO2 = 650, Humidity = 100, Time = DateTime.Now };
+            meassurement5 = new Meassurement() { Id = 5, CO2 = 650, Humidity = 100, Location = "Her" };
+            meassurement6 = new Meassurement() { Id = 6, CO2 = 650, Humidity = -1, Location = "Her", Time = DateTime.Now };
+
+        }
         [TestMethod()]
         public void ValidateTest()
         {
-            Assert.Fail();
+            meassurement1.Validate();
+            Assert.ThrowsException<Exception>(()=>meassurement2.Validate());
+            Assert.ThrowsException<Exception>(() => meassurement3.Validate());
+            Assert.ThrowsException<Exception>(() => meassurement4.Validate());
+            Assert.ThrowsException<Exception>(() => meassurement5.Validate());
+            Assert.ThrowsException<Exception>(() => meassurement6.Validate());
         }
     }
 }
